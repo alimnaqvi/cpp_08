dd9b1b80f0128308c2f9159ab81032d923f43780
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-05-24 11:53:42 +0200

    return const_iterator instead of iterator so const containers can be passed as well

--- ex00/easyfind.h
+++ ex00/easyfind.h
@@ -6,11 +6,11 @@
 #include <iterator>
 #include <string>
 
-/* Look for @toFind in @container and return an iterator pointing to it if found, else return end() */
+/* Look for @toFind in @container and return a const iterator pointing to it if found, else return end() */
 template <typename T>
-typename T::iterator easyfind( T& container, int toFind )
+typename T::const_iterator easyfind( T& container, const int toFind )
 {
-    return std::find( container.begin(), container.end(), toFind );
+    return std::find( container.cbegin(), container.cend(), toFind );
 }
 
 /* Helper function template to print all elements of a container */
@@ -19,18 +19,16 @@ void printContainer( T& container )
 {
     std::cout << "{ ";
     for ( const auto& elem : container )
-    {
         std::cout << elem << ' ';
-    }
     std::cout << "}\n";
 }
 
 /* Helper function template to print found message */
 template <typename T>
-void printFound( T& container, int toFind, typename T::iterator& found, std::string_view containerName )
+void printFound( T& container, const int toFind, typename T::const_iterator& found, std::string_view containerName )
 {
-    if ( found != container.end() )
-        std::cout << "Found " << *found << " at index " << found - container.begin() << ".\n";
+    if ( found != container.cend() )
+        std::cout << "Found " << *found << " at index " << found - container.cbegin() << ".\n";
     else
         std::cout << toFind << " could not be found in " << containerName << ".\n";
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -13,19 +13,32 @@ int main()
     // std::vector
     toFind = 11;
     containerName.assign( "std::vector primes" );
-    std::vector primes{ 2, 3, 5, 7, 11, 13, 17 };
+    std::vector<int> primes{ 2, 3, 5, 7, 11, 13, 17 };
 
     std::cout << "Finding " << toFind << " in " << containerName << ":\n";
     printContainer( primes );
 
-    auto found{ easyfind( primes, toFind ) };
+    auto foundVector{ easyfind( primes, toFind ) };
 
-    printFound( primes, toFind, found, containerName );
-    // found = std::find( primes.begin(), primes.end(), toFind );
+    printFound( primes, toFind, foundVector, containerName );
+    // foundVector = std::find( primes.begin(), primes.end(), toFind );
 
     std::cout << "----------------------------------" << '\n';
 
-    // std::array
+    // const std::array
+    toFind = 42;
+    containerName.assign( "std::array fortyTwo" );
+    const std::array<int, 5> fortyTwo{ 42, 42, 42, 42, 42 };
+
+    std::cout << "Finding " << toFind << " in " << containerName << ":\n";
+    printContainer( fortyTwo );
+
+    auto foundArray{ easyfind( fortyTwo, toFind ) };
+
+    printFound( fortyTwo, toFind, foundArray, containerName );
+    // foundArray = std::find( fortyTwo.begin(), fortyTwo.end(), toFind );
+
+    std::cout << "----------------------------------" << '\n';
 
     // std::list
 
